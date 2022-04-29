499955a16a618af6c9002377b8d0b43cfb6771e3
Alex Younger <alexyounger07@gmail.com>
2022-04-29 15:22:21 -0400

    GraphQL is working

--- Gemfile
+++ Gemfile
@@ -52,3 +52,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "graphql", "~> 2.0"
+
+gem "bcrypt", "~> 3.1"
+
+gem 'graphiql-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  [
+    {
+      name: 'Luke Skywalker',
+      username: 'lukesky',
+      password: 'iamyourfather'
+    },
+    {
+      name: 'Lando Calrissian',
+      username: 'lando',
+      password: 'dealgetsworseallthetime'
+    },
+    {
+      name: 'Paul Atreides',
+      username: 'paul',
+      password: 'fearisthemindkiller'
+    },
+    {
+      name: 'Stilgar',
+      username: 'stilgar',
+      password: 'youwillwasteyourbodyswater'
+    }
+  ]
+)
